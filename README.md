407fa0f587d97d22d38dc80cc8755f7490b0008b
Andrey Denisov <a.denisov@office.ngs.ru>
2014-12-24 15:04:25 +0600

    Quick fix to support timestamps

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ python-gcm-dummy
 Dummy server that simulates apple APNS feedback service, it is designed to easy make an integration test in order to verify if tokens are
 properly managed 
 
-At this point timestamp is not correctly sended
 
 Responses
 -------------
@@ -11,8 +10,8 @@ By default server will output ok responses but a results file can be defined.
 
 Example file:
 ```
-someapplepushtoken1
-someapplepushtoken2
+0123456780123456780123456780123456780123456780001234567812345678, 1419406068
+0123456780123456780123456780123456780123456780001234567812345679, 1419405068
 ```
 
 Server will automatically load any resuts.csv file in the working dir.
--- server.py
+++ server.py
@@ -5,22 +5,26 @@ import os
 from optparse import OptionParser
 from gevent import monkey; monkey.patch_all()
 from gevent.server import StreamServer
+import csv
 
 cert = os.path.join(os.path.dirname(__file__), 'server.crt')
 key = os.path.join(os.path.dirname(__file__), 'server.key')
-results = {}
+results = []
 
-def response_by_token(token):
+
+def response_by_token(token, timestamp):
     token_bin = a2b_hex(token)
     token_length_bin = pack('>H', (len(token_bin)))
-    date = '1234' # need to correctly simulate 4byte timestamp
+    date = pack('>I', timestamp)
     return date + token_length_bin + token_bin
+
 
 def handle(socket, address):
     print 'New connection from %s:%s' % address
 
     for token in results:
-        socket.sendall(response_by_token(token))
+        socket.sendall(response_by_token(token['token'], int(token['timestamp'])))
+
 
 def main():
     parser = OptionParser()
@@ -48,10 +52,11 @@ def main():
     if os.path.exists(options.rfile):
         print 'Loaded %s file' % options.rfile
         global results
-        results = [x.strip() for x in open(options.rfile, 'r') if x.strip()]
+        with open(options.rfile, mode='r') as csv_file:
+            reader = csv.reader(csv_file)
+            results = [{'token': row[0], 'timestamp': row[1]} for row in reader]
     else:
         print 'No results file loaded, sever will accept connections but no feedback will be sended'
-
 
     print "Starting server on %s:%s" % (options.bind, int(options.port))
     StreamServer((options.bind, int(options.port)), handle, keyfile=key, certfile=cert).serve_forever()
